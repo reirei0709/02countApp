dcc2b0c4d9c72c9f3376732d9c60d71877a35201
mirei <usrmii301@gmail.com>
2021-02-01 16:39:06 +0900

    finish

---
+++ app/src/main/java/app/itakura/reirei/a02countapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package app.itakura.reirei.a02countapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number:Int = 0
+
+        numberTextView.text = "0"
+
+        plusButton.setOnClickListener {
+            number +=1
+
+            numberTextView.text = number.toString()
+
+            if (number % 3 == 0){
+               numberTextView.setTextColor(Color.GREEN)
+            }else{
+                numberTextView.setTextColor(Color.BLACK)
+            }
+        }
+
+        minusButton.setOnClickListener {
+            number -=1
+
+            numberTextView.text = number.toString()
+
+            if (number % 3 == 0){
+                numberTextView.setTextColor(Color.GREEN)
+            }else{
+                numberTextView.setTextColor(Color.BLACK)
+            }
+        }
+
+        clearButton.setOnClickListener {
+            number = 0
+
+            numberTextView.text = number.toString()
+
+            if (number % 3 == 0){
+                numberTextView.setTextColor(Color.GREEN)
+            }else{
+                numberTextView.setTextColor(Color.BLACK)
+            }
+        }
+
+
+
+    }
+}
